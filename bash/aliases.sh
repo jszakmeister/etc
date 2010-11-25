99c39f5be00fc952356b2002d0c96ffe25b3fd20
John Szakmeister <john@szakmeister.net>
2010-11-25 08:19:39 -0500

    fixup how an installed clojure is found.  also tweak the JVM settings to avoid a warning.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -55,12 +55,35 @@ if [ -d $HOME/.local/erlang ]; then
         alias erlc="$HOME/local/erlang/bin/erlc"
 fi
 
-if [ -f $JAVA_LOCALLIB/clojure.jar ]; then
-        alias clj="java -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -cp $JAVA_LOCALLIB/jline.jar:$JAVA_LOCALLIB/clojure.jar:$JAVA_LOCALLIB/clojure-contrib.jar jline.ConsoleRunner clojure.main"
+# Hunt down the installed clojure files
+search_paths=$(echo $JAVA_LOCALLIB | tr ":" "\n")
+clojure_jar=
+clojure_contrib_jar=
+jline_jar=
+
+for search_path in $search_paths
+do
+    if [ -f $search_path/clojure.jar ]; then
+        clojure_jar="$search_path/clojure.jar"
+        clojure_contrib_jar="$search_path/clojure-contrib.jar"
+        break
+    fi
+done
+
+for search_path in $search_paths
+do
+    if [ -f $search_path/jline.jar ]; then
+        jline_jar="$search_path/jline.jar"
+        break
+    fi
+done
+
+if [[ "$clojure_jar" != '' ]]; then
+        alias clj="java -XX:+CMSClassUnloadingEnabled -XX:+CMSClassUnloadingEnabled -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -cp $jline_jar:$clojure_jar:$clojure_contrib_jar jline.ConsoleRunner clojure.main"
 fi
 
 if [ -d $HOME/projects/clojure ]; then
-        alias dev-clj="java -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -cp $JAVA_LOCALLIB/jline.jar:$HOME/projects/clojure/clojure.jar:$HOME/clojure-contrib.jar jline.ConsoleRunner clojure.main"
+        alias dev-clj="java -XX:+CMSClassUnloadingEnabled -XX:+CMSClassUnloadingEnabled -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -cp $JAVA_LOCALLIB/jline.jar:$HOME/projects/clojure/clojure.jar:$HOME/projects/clojure-contrib/modules/complete/target/clojure-contrib-*.jar jline.ConsoleRunner clojure.main"
 fi
 
 alias wget="wget --no-check-certificate"
