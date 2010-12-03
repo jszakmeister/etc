e5c3cd9a7a4975f130f2e5bb18f34dd170436826
John Szakmeister <john@szakmeister.net>
2010-12-02 21:08:22 -0500

    add a textedit alias for Mac OS X

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -22,6 +22,7 @@ if [[ "$platform" == 'Darwin' ]]; then
                 alias qtd='open -a /Developer/Applications/Qt/Designer.app'
         fi
         alias keychain='open -a /Applications/Utilities/Keychain\ Access.app'
+        alias textedit='open -a /Application/TextEdit.app'
 fi
 
 if [[ "$platform" == 'Linux' ]]; then
