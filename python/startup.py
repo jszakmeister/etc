6e47bc78b2d7f00b3a2ac2a7f7d84f40d923d72e
John Szakmeister <john@szakmeister.net>
2011-02-20 10:44:47 -0500

    Avoid calling read_init_file().
    Instead, just make our own choice.  Bind the search history if
    .editrc doesn't exist.  Always bind rl_complete... as it doesn't
    seem to care about what's specified in .editrc.

--- python/startup.py
+++ python/startup.py
@@ -2,16 +2,16 @@
 #  export PYTHONSTARTUP="$HOME/projects/etc/python/startup.py"
 
 import readline, atexit, os, rlcompleter
- 
+
+
 historypath = os.path.expanduser("~/.pyhistory")
 editrcpath = os.path.expanduser("~/.editrc")
 
-try:
-    readline.read_init_file(editrcpath)
-except:
-    readline.parse_and_bind("bind ^I rl_complete")
+if not os.path.exists(editrcpath):
     readline.parse_and_bind("bind ^[[A ed-search-prev-history")
     readline.parse_and_bind("bind ^[[B ed-search-next-history")
+
+readline.parse_and_bind("bind ^I rl_complete")
 
 def save_history(historypath=historypath):
     import readline
