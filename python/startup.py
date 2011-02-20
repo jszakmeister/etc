3b361e722911986cacaff989cdd57933ec0e9592
John Szakmeister <john@szakmeister.net>
2011-02-20 10:25:39 -0500

    Try to read .editrc.
    Seems to fail on Mac OS X... but doesn't seem like it should be.
    Needs more investigation.

--- python/startup.py
+++ python/startup.py
@@ -4,9 +4,14 @@
 import readline, atexit, os, rlcompleter
  
 historypath = os.path.expanduser("~/.pyhistory")
-readline.parse_and_bind("bind ^I rl_complete")
-readline.parse_and_bind("bind ^[[A ed-search-prev-history")
-readline.parse_and_bind("bind ^[[B ed-search-next-history")
+editrcpath = os.path.expanduser("~/.editrc")
+
+try:
+    readline.read_init_file(editrcpath)
+except:
+    readline.parse_and_bind("bind ^I rl_complete")
+    readline.parse_and_bind("bind ^[[A ed-search-prev-history")
+    readline.parse_and_bind("bind ^[[B ed-search-next-history")
 
 def save_history(historypath=historypath):
     import readline
@@ -17,5 +22,5 @@ if os.path.exists(historypath):
                 
 atexit.register(save_history)
                  
-del os, atexit, readline, save_history, historypath, rlcompleter
+del os, atexit, readline, save_history, historypath, editrcpath, rlcompleter
 
