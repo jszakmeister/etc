427f92c74a3b8dc39f1b8ef4eb32b62f37bcb8a9
John Szakmeister <john@szakmeister.net>
2011-04-18 05:36:33 -0400

    Bring in git and ssh completions.

---
+++ bash/completions.sh
@@ -0,0 +1,2 @@
+test -s ~/projects/etc/bash/git-autocomplete.sh && . ~/projects/etc/bash/git-autocomplete.sh
+test -s ~/projects/etc/bash/ssh-autocomplete.sh && . ~/projects/etc/bash/ssh-autocomplete.sh
--- bash/git-autocomplete.sh
+++ bash/git-autocomplete.sh
@@ -1,4 +1,6 @@
 # Point at the location of your bash completion file
 if [ -f $HOME/.local/etc/git-completion.bash ]; then
    . $HOME/.local/etc/git-completion.bash
+elif [ -f $HOME/.local/etc/completions/git-completion.bash ]; then
+   . $HOME/.local/etc/completions/git-completion.bash
 fi
