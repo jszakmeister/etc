618deb4a7afb8746b0033ef6f77b5a6288355e3b
John Szakmeister <john@szakmeister.net>
2011-04-20 15:37:09 -0400

    A little too quick deleting lines.  Still need the alias for emacs.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -28,6 +28,7 @@ if [[ "$platform" == 'Darwin' ]]; then
         alias keychain='open -a /Applications/Utilities/Keychain\ Access.app'
         alias textedit='open -a /Applications/TextEdit.app'
         if [ -d $HOME/Applications/Emacs.app ]; then
+                alias emacs="$HOME/Applications/Emacs.app/Contents/MacOS/Emacs"
                 alias em="$HOME/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
                 alias ec="$HOME/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
         fi
