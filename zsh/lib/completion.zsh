6e4c721d39e28557378ffd1790682f294001d428
John Szakmeister <john@szakmeister.net>
2011-07-21 06:15:09 -0400

    Setup a completion cache directory.

--- zsh/lib/completion.zsh
+++ zsh/lib/completion.zsh
@@ -2,3 +2,7 @@
 # Here's a bug report on the issue:
 #    http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=514152
 zstyle ':completion:*' special-dirs true
+
+# Setup a cache so that apt and dpkg completions are usable
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path ~/.zcache
