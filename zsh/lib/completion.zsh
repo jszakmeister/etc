e9dc6b9ece9926893e2c27b77456d7756387f148
John Szakmeister <john@szakmeister.net>
2011-07-21 06:11:06 -0400

    Work around a bug with .. completion.

---
+++ zsh/lib/completion.zsh
@@ -0,0 +1,4 @@
+# Turns on completion for . and .. directories.
+# Here's a bug report on the issue:
+#    http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=514152
+zstyle ':completion:*' special-dirs true
