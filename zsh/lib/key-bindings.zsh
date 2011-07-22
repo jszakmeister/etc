b0f6752a0e8e4348991501f8b347c43aad23f7aa
John Szakmeister <john@szakmeister.net>
2011-07-22 06:26:05 -0400

    Get more bash-like history search with the up and down arrows.

--- zsh/lib/key-bindings.zsh
+++ zsh/lib/key-bindings.zsh
@@ -27,10 +27,16 @@ unset k
 # default to emacs bindings, just like bash
 bindkey -e
 
+autoload -U up-line-or-beginning-search
+zle -N up-line-or-beginning-search
+
+autoload -U down-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
 # setup key accordingly
 [[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
-[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      history-beginning-search-backward
-[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    history-beginning-search-forward
+[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-beginning-search
+[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-beginning-search
 [[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
 [[ -n "${key[CLeft]}"   ]]  && bindkey  "${key[CLeft]}"   backward-word
 [[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
