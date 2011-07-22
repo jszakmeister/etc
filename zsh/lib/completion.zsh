74f169abcf73ca2add5a91f01cea9c160cfdfe9a
John Szakmeister <john@szakmeister.net>
2011-07-21 21:51:59 -0400

    Add a few completion-related settings.

--- zsh/lib/completion.zsh
+++ zsh/lib/completion.zsh
@@ -6,3 +6,10 @@ zstyle ':completion:*' special-dirs true
 # Setup a cache so that apt and dpkg completions are usable
 zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path ~/.zcache
+
+# Remove the trailing slash when a directory is used as an argument
+zstyle ':completion:*' squeeze-slashes true
+
+set show-all-if-ambiguous on
+set completion-query-items 1000
+set expand-tilde off
