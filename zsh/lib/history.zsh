8cad54ebdefd24229d97676f4d67f6b255e75afa
John Szakmeister <john@szakmeister.net>
2011-09-07 06:27:35 -0400

    Clarify the history-related options that are being set.

--- zsh/lib/history.zsh
+++ zsh/lib/history.zsh
@@ -3,14 +3,30 @@ HISTFILE=$HOME/.zsh-history
 HISTSIZE=100000
 SAVEHIST=100000
 
-setopt hist_ignore_dups # ignore duplication command history list
-setopt share_history # share command history data
+# Ignore duplication command history list
+setopt hist_ignore_dups
 
-setopt hist_verify
+# Share command history data
+setopt share_history
+
+# Don't just expand... expand and execute
+unsetopt hist_verify
+
+# Incrementally add history to the history file.
+# share_history does something similar too.
 setopt inc_append_history
+
+# Save timestamp in history file
 setopt extended_history
+
+# Shouldn't matter because of hist_ignore_dups,
+# but if they do appear, expire dups first.
 setopt hist_expire_dups_first
+
+# Don't record commands that begin with a space.
+# You'll still have the command in local history,
+# it just isn't written to disk.
 setopt hist_ignore_space
 
-setopt SHARE_HISTORY
-setopt APPEND_HISTORY
+# Don't store invocations of the history command
+setopt hist_no_store
