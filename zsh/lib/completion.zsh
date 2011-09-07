4e6a791aa99236a0eb2dfe79e3fccdf23882617e
John Szakmeister <john@szakmeister.net>
2011-09-07 06:39:11 -0400

    Don't move the cursor to the last prompt when completing!

--- zsh/lib/completion.zsh
+++ zsh/lib/completion.zsh
@@ -19,5 +19,9 @@ set hash_list_all
 # show the completion list right away
 setopt auto_list
 
+# Don't take us to the last prompt on completion.  This brings
+# it more in-line with what bash does.
+unsetopt always_last_prompt
+
 # don't prompt for less than 1000 completions
 LISTMAX=1000
