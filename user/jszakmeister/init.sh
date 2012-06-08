a126fc3477f5be516f7b4b3ff502752746a78cba
John Szakmeister <john@szakmeister.net>
2012-06-08 06:08:57 -0400

    completion: make a couple of tweaks to _make_dir_complete
    I tend to use a symlink for ~/projects, so that I can put my files on a
    case-sensitive volume under Mac OS X.  However, my cdp alias wouldn't
    complete for files under ~/projects because it was a link.  Instead, use
    the logical path for the cd command, and use the realpath for completion
    only.
    
    Also, it turns out cdc has some completion assigned to it already, so
    unset it in my config before defining cdc.

--- user/jszakmeister/init.sh
+++ user/jszakmeister/init.sh
@@ -1,4 +1,10 @@
 _make_dir_complete cdi ~/projects/intelesys
 _make_dir_complete cdj ~/projects/jszakmeister
-_make_dir_complete cdc ~/projects/clojure
 _make_dir_complete cdp ~/projects
+
+# In ZSH, we need to remove any completions associated with cdc, or this will
+# fail.
+if [ -n "$ZSH_VERSION" ]; then
+    compdef -d cdc
+fi
+_make_dir_complete cdc ~/projects/clojure
