d8203355e4dfaa184b7c6084c3139c2f0875898d
John Szakmeister <john@szakmeister.net>
2012-06-14 10:40:39 -0400

    init: check that the directory exists before creating the shortcut

--- user/jszakmeister/init.sh
+++ user/jszakmeister/init.sh
@@ -1,10 +1,14 @@
-_make_dir_complete cdi ~/projects/intelesys
-_make_dir_complete cdj ~/projects/jszakmeister
-_make_dir_complete cdp ~/projects
+test -e ~/projects/intelesys &&
+    _make_dir_complete cdi ~/projects/intelesys
+test -e ~/projects/jszakmeister &&
+    _make_dir_complete cdj ~/projects/jszakmeister
+test -e ~/projects &&
+    _make_dir_complete cdp ~/projects
 
 # In ZSH, we need to remove any completions associated with cdc, or this will
 # fail.
 if [ -n "$ZSH_VERSION" ]; then
     compdef -d cdc
 fi
-_make_dir_complete cdc ~/projects/clojure
+test -e ~/projects/clojure &&
+    _make_dir_complete cdc ~/projects/clojure
