61374562244de21d749836deabeea013286e834a
John Szakmeister <john@szakmeister.net>
2012-06-18 19:41:06 -0400

    shell: allow passing a command to _make_dir_complete
    Update the calls to it, and add a few more defining shortcuts for using
    pushd.

--- user/jszakmeister/init.sh
+++ user/jszakmeister/init.sh
@@ -1,9 +1,12 @@
 test -e ~/projects/intelesys &&
-    _make_dir_complete cdi ~/projects/intelesys
+    _make_dir_complete cdi cd ~/projects/intelesys &&
+    _make_dir_complete pdi pushd ~/projects/intelesys
 test -e ~/projects/jszakmeister &&
-    _make_dir_complete cdj ~/projects/jszakmeister
+    _make_dir_complete cdj cd ~/projects/jszakmeister &&
+    _make_dir_complete pdj pushd ~/projects/jszakmeister
 test -e ~/projects &&
-    _make_dir_complete cdp ~/projects
+    _make_dir_complete cdp cd ~/projects &&
+    _make_dir_complete pdp pushd ~/projects
 
 # In ZSH, we need to remove any completions associated with cdc, or this will
 # fail.
@@ -11,4 +14,5 @@ if [ -n "$ZSH_VERSION" ]; then
     compdef -d cdc
 fi
 test -e ~/projects/clojure &&
-    _make_dir_complete cdc ~/projects/clojure
+    _make_dir_complete cdc cd ~/projects/clojure &&
+    _make_dir_complete pdc pushd ~/projects/clojure
