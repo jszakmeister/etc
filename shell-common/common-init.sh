ce4884a6095e4f60f434b4f718372fe5eb21e532
John Szakmeister <john@szakmeister.net>
2012-10-04 14:56:14 -0400

    common-init: do a better check for virtualenvwrapper

--- shell-common/common-init.sh
+++ shell-common/common-init.sh
@@ -1,3 +1,5 @@
-if [ "$(which virtualenvwrapper.sh)" != "" ]; then
-    source "$(which virtualenvwrapper.sh)"
+virtualenvwrapper_path=$(which virtualenvwrapper.sh)
+if test $? -eq 0; then
+    source "${virtualenvwrapper_path}"
 fi
+unset virtualenvwrapper_path
