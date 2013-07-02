3b5d1c5a618daa709c3a4bcba370129c74f92be2
John Szakmeister <john@szakmeister.net>
2013-07-02 05:34:14 -0400

    python: some small cleanups to startup.py

--- python/startup.py
+++ python/startup.py
@@ -16,11 +16,10 @@ readline.parse_and_bind("bind ^I rl_complete")
 def save_history(historypath=historypath):
     import readline
     readline.write_history_file(historypath)
-           
+
 if os.path.exists(historypath):
     readline.read_history_file(historypath)
-                
-atexit.register(save_history)
-                 
-del os, atexit, readline, save_history, historypath, editrcpath, rlcompleter
 
+atexit.register(save_history)
+
+del os, atexit, readline, save_history, historypath, editrcpath, rlcompleter
