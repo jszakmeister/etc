a22620ffae7fca39ea645078f4dda057c743e171
John Szakmeister <john@szakmeister.net>
2013-12-20 17:28:58 -0500

    bash: set up some history-related options

--- bash/shell-options.sh
+++ bash/shell-options.sh
@@ -8,3 +8,10 @@ PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
 
 # Make sure COLUMNS is set correctly
 shopt -s checkwinsize
+
+# Don't store duplicates and ignore commands with a leading space
+export HISTCONTROL=ignoreboth
+
+# Remember 100,000 lines of history
+export HISTSIZE=100000
+export HISTFILESIZE=10000
