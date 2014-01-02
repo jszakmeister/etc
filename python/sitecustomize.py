15fb6defc374e014a16097fc8b9df6d21417160f
John Szakmeister <john@szakmeister.net>
2014-01-02 05:10:49 -0500

    python: add a couple of comments

--- python/sitecustomize.py
+++ python/sitecustomize.py
@@ -9,6 +9,9 @@ import site
 p = os.path.expanduser(
         '~/.local/lib/python%s/site-packages' % platform.python_version().rsplit('.', 1)[0])
 if os.path.exists(p):
+    # Make sure the path is treated as a site directory
     site.addsitedir(p)
+    # Most importantly, make sure it's near the front of sys.path... which isn't
+    # necessarily the case on some platforms.
     sys.path.insert(0, p)
 del p
