c84c343f093844a9230f0059d9e17832a9dedfd5
John Szakmeister <john@szakmeister.net>
2014-01-02 04:54:09 -0500

    shell-common: replace all uses of backticks with $(...)

--- shell-common/exports.sh
+++ shell-common/exports.sh
@@ -15,38 +15,38 @@ test -d "$HOME/local/bin" &&
 if [[ "$platform" == 'darwin' ]]; then
         python_version=$(python -c "import sys; print '%d.%d' % sys.version_info[:2]" 2>/dev/null ||
                          python -c "import sys; print('{}.{}'.format(*sys.version_info[:2]))" 2>/dev/null)
-        PATHS_TO_PREPEND=`append_path "$PATHS_TO_PREPEND" "$HOME/Library/Python/$python_version/bin"`
-        PATHS_TO_APPEND=`append_path "$PATHS_TO_APPEND" "/Library/Python/$python_version/bin:/System/Library/Frameworks/Python.framework/Versions/$python_version/bin"`
+        PATHS_TO_PREPEND=$(append_path "$PATHS_TO_PREPEND" "$HOME/Library/Python/$python_version/bin")
+        PATHS_TO_APPEND=$(append_path "$PATHS_TO_APPEND" "/Library/Python/$python_version/bin:/System/Library/Frameworks/Python.framework/Versions/$python_version/bin")
 
-        slickedit_path=`\ls -d /Applications/SlickEdit* ~/Applications/SlickEdit* 2>/dev/null | sort -rn | head -n 1`
+        slickedit_path=$(\ls -d /Applications/SlickEdit* ~/Applications/SlickEdit* 2>/dev/null | sort -rn | head -n 1)
         if [[ $slickedit_path != '' ]]; then
             if [ -f $slickedit_path/Contents/slickedit/bin/vs ]; then
-                PATHS_TO_APPEND=`append_path "$PATHS_TO_APPEND" "$slickedit_path/Contents/slickedit/bin"`
+                PATHS_TO_APPEND=$(append_path "$PATHS_TO_APPEND" "$slickedit_path/Contents/slickedit/bin")
             fi
             if [ -f $slickedit_path/Contents/MacOS/vs ]; then
-                PATHS_TO_APPEND=`append_path "$PATHS_TO_APPEND" "$slickedit_path/Contents/MacOS"`
+                PATHS_TO_APPEND=$(append_path "$PATHS_TO_APPEND" "$slickedit_path/Contents/MacOS")
             fi
         fi
 
         if [ -d /opt/local/bin ]; then
-                PATHS_TO_APPEND=`append_path "$PATHS_TO_APPEND" /opt/local/bin`
+                PATHS_TO_APPEND=$(append_path "$PATHS_TO_APPEND" /opt/local/bin)
         fi
 
         if [ -d /usr/local/texlive/2009/bin/universal-darwin ]; then
-                PATHS_TO_APPEND=`append_path "$PATHS_TO_APPEND" "/usr/local/texlive/2009/bin/universal-darwin"`
+                PATHS_TO_APPEND=$(append_path "$PATHS_TO_APPEND" "/usr/local/texlive/2009/bin/universal-darwin")
         fi
 fi
 if [[ "$platform" == 'linux' ]]; then
         if [ -d /opt/slickedit ]; then
-                PATHS_TO_PREPEND=`append_path "$PATHS_TO_PREPEND" /opt/slickedit/bin`
+                PATHS_TO_PREPEND=$(append_path "$PATHS_TO_PREPEND" /opt/slickedit/bin)
         fi
         if [ -d $HOME/.local/slickedit ]; then
-                PATHS_TO_PREPEND=`append_path "$PATHS_TO_PREPEND" "$HOME/.local/slickedit/bin"`
+                PATHS_TO_PREPEND=$(append_path "$PATHS_TO_PREPEND" "$HOME/.local/slickedit/bin")
         fi
 fi
 
 if [ -d "$ETC_HOME/git-addons" ]; then
-        PATHS_TO_PREPEND=`append_path "$PATHS_TO_PREPEND" "$ETC_HOME/git-addons"`
+        PATHS_TO_PREPEND=$(append_path "$PATHS_TO_PREPEND" "$ETC_HOME/git-addons")
 fi
 
 if [ -d "$ETC_HOME/scripts" ]; then
@@ -140,10 +140,10 @@ export EDITOR=$(_find_executable vim)
 # Add a default LS_COLORS
 if [ "$TERM" != "dumb" ]; then
   if [ -x /usr/bin/dircolors ]; then
-    eval "`dircolors -b`"
+      eval "$(dircolors -b)"
   else
-    LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:';
-    export LS_COLORS
+      LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.lz=01;31:*.xz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.axv=01;35:*.anx=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.axa=00;36:*.oga=00;36:*.spx=00;36:*.xspf=00;36:';
+      export LS_COLORS
   fi
 fi
 
