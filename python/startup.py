ba6643afdcb73f9ca8d46b58c9fa15bfc738d00a
John Szakmeister <john@szakmeister.net>
2014-02-08 06:09:17 -0500

    Cope with libedit vs. readline issues in startup.py.
    Not only does libedit use a different mechanism for binding, it also
    uses a different history format.  To complicate matters more, Mac's
    system python lies about whether it's using readline or libedit.  To be
    precise, it seems Apple has chosen to build Python using the readline
    compatibility library rather than exposing libedit outright.  This
    complicates matters for detection purposes.  So we now look to see if
    the readline module is in the system area on the Mac, and if so, treat
    it libedit.  It's not a perfect heuristic because of the libedit's
    readline compatibility bindings, but it's the best we've got.
    
    Note: we can no longer switch on just the platform type because things
    like Homebrew or MacPorts may use actual readline vs. the native
    libedit.

--- python/startup.py
+++ python/startup.py
@@ -1,35 +1,53 @@
 # Add a line like the following to your .bashrc:
 #  export PYTHONSTARTUP="$ETC_HOME/python/startup.py"
 
-import readline, atexit, os, rlcompleter, sys
 
+def setup_readline():
+    try:
+        import readline, atexit, os, rlcompleter, sys
 
-historypath = os.path.expanduser("~/.pyhistory")
-editrcpath = os.path.expanduser("~/.editrc")
-inputrcpath = os.path.expanduser("~/.inputrc")
+        if 'libedit' in readline.__doc__:
+            is_libedit = True
+        else:
+            # System Python on Mac's lie about being readline.  They're really
+            # libedit.
+            is_libedit = False
+            if sys.platform == 'darwin' and 'Apple' in sys.version:
+                if readline.__file__.startswith(
+                        '/System/Library/Frameworks/Python.framework/'):
+                    is_libedit = True
 
-if 'darwin' in sys.platform:
-    if not os.path.exists(editrcpath):
-        readline.parse_and_bind("bind ^[[A ed-search-prev-history")
-        readline.parse_and_bind("bind ^[[B ed-search-next-history")
+        suffix = '-' + '.'.join(str(x) for x in sys.version_info[:2])
+        suffix += (is_libedit and "-el") or "-rl"
+        historypath = os.path.expanduser("~/.pyhistory" + suffix)
+        editrcpath = os.path.expanduser("~/.editrc")
+        inputrcpath = os.path.expanduser("~/.inputrc")
 
-    readline.parse_and_bind("bind ^I rl_complete")
+        if is_libedit:
+            if not os.path.exists(editrcpath):
+                readline.parse_and_bind("bind ^[[A ed-search-prev-history")
+                readline.parse_and_bind("bind ^[[B ed-search-next-history")
 
-elif 'linux' in sys.platform:
-    if not os.path.exists(inputrcpath):
-        readline.parse_and_bind(r'"\e[A": history-search-backward')
-        readline.parse_and_bind(r'"\e[B": history-search-forward')
+            readline.parse_and_bind("bind ^I rl_complete")
+        else:
+            if not os.path.exists(inputrcpath):
+                readline.parse_and_bind(r'"\e[A": history-search-backward')
+                readline.parse_and_bind(r'"\e[B": history-search-forward')
 
-    readline.parse_and_bind("tab: complete")
+            readline.parse_and_bind("tab: complete")
 
-def save_history(historypath=historypath):
-    import readline
-    readline.write_history_file(historypath)
+        def save_history(historypath=historypath):
+            import readline
+            readline.write_history_file(historypath)
 
-if os.path.exists(historypath):
-    readline.read_history_file(historypath)
+        if os.path.exists(historypath):
+            readline.read_history_file(historypath)
 
-atexit.register(save_history)
+        atexit.register(save_history)
 
-del os, sys, atexit, readline, save_history, historypath
-del inputrcpath, editrcpath, rlcompleter
+    except ImportError:
+        pass
+
+setup_readline()
+
+del setup_readline
