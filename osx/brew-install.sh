1b7679a1425c9eb499b5c092c510bdbc6006b2c9
John Szakmeister <john@szakmeister.net>
2014-08-03 09:04:35 -0400

    osx: add more brew packages to install

--- osx/brew-install.sh
+++ osx/brew-install.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 PACKAGES_TO_INSTALL='
+    asciidoc
     autoconf
     automake
     ccache
     colordiff
+    gawk
     gettext
+    grep
+    httpie
     libdvdcss
     libtool
     md5sha1sum
     minicom
+    nasm
+    netcat
     ninja
     nmap
     patchutils
     pkg-config
+    quilt
     rsync
+    tig
     tmux
     tree
     wget
+    xmlto
+    xmltoman
     xz
 '
 
@@ -25,3 +35,5 @@ for package in ${PACKAGES_TO_INSTALL}; do
 done
 
 brew install ctags --keep-ctags
+brew install homebrew/dupes/openssh --with-keychain-support --with-ldns
+brew install zmq --universal --with-pgm
