7ab3466b317441a04b733d7187fc38ad0be87688
John Szakmeister <john@szakmeister.net>
2014-10-14 18:52:03 -0400

    user/jszakmeister: colorize zsh scripts

--- user/jszakmeister/lessfilter.sh
+++ user/jszakmeister/lessfilter.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 case "$1" in
-    *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
+    *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|*.zsh|\
     *.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
     *.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
     *.py|*.rb|*.sql|*.ebuild|*.eclass|*.vim)
         pygmentize -f 256 -O style=native "$1" 2>/dev/null;;
-    .bashrc|.bash_aliases|.bash_environment)
+    .bashrc|.bash_aliases|.bash_environment|.zshrc|.zshenv|.zsh-history)
         pygmentize -f 256 -O style=native -l sh "$1" 2>/dev/null
         ;;
     *.patch|*.diff)
