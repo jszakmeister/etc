8feb922a1002fe1103af2fa7a818c0f2dd074e8e
John Szakmeister <john@szakmeister.net>
2014-11-16 05:19:48 -0500

    user/jszakmeister: set the output encoding while using pygmentize
    Some of my files have UTF-8 characters, and pygmentize will exit with an
    error when it tries to convert to Python's default setting of
    sys.stdout.encoding--which is 'ascii'.

--- user/jszakmeister/lessfilter.sh
+++ user/jszakmeister/lessfilter.sh
@@ -3,30 +3,32 @@ if ! command -v pygmentize > /dev/null 2>&1; then
     exit 1
 fi
 
+PYGMENTIZE="pygmentize -f 256 -O style=native,outencoding=utf-8"
+
 case "$(basename $1)" in
     *.awk|*.groff|*.java|*.js|*.m4|*.php|*.pl|*.pm|*.pod|*.sh|\
     *.ad[asb]|*.asm|*.inc|*.[ch]|*.[ch]pp|*.[ch]xx|*.cc|*.hh|\
     *.lsp|*.l|*.pas|*.p|*.xml|*.xps|*.xsl|*.axp|*.ppd|*.pov|\
     *.py|*.rb|*.sql|*.ebuild|*.eclass|*.vim|*.cmake|CMakeLists.txt|Makefile)
-        pygmentize -f 256 -O style=native "$1" 2>/dev/null
+        ${PYGMENTIZE} "$1" 2>/dev/null
         ;;
     *.mk)
-        pygmentize -f 256 -O style=native -l make "$1" 2>/dev/null
+        ${PYGMENTIZE} -l make "$1" 2>/dev/null
         ;;
     .bashrc|.bash_aliases|.bash_environment|*.zsh|.zshrc|.zshenv|.zsh-history)
-        pygmentize -f 256 -O style=native -l sh "$1" 2>/dev/null
+        ${PYGMENTIZE} -l sh "$1" 2>/dev/null
         ;;
     *.patch|*.diff)
         if hash colordiff > /dev/null 2>&1; then
             cat "$1" | colordiff | diff-highlight
         else
-            pygmentize -f 256 -O style=native "$1" 2>/dev/null | diff-highlight
+            ${PYGMENTIZE} "$1" 2>/dev/null | diff-highlight
         fi
         ;;
     *)
         grep -E "#\!/bin/(bash|sh|zsh)" "$1" > /dev/null
         if [ "$?" -eq "0" ]; then
-            pygmentize -f 256 -O style=native -l sh "$1" 2>/dev/null
+            ${PYGMENTIZE} -l sh "$1" 2>/dev/null
         else
             exit 1
         fi
