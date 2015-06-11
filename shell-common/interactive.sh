e59dd973d44a4cbf73d4c379efe2c7526276bf91
John Szakmeister <john@szakmeister.net>
2015-06-11 06:46:20 -0400

    shell-common: look for virtualenvwrapper in /usr/share
    In later versions of Ubuntu, virtualenvwrapper is packaged in /usr/share
    instead of being on the path.  Let's look for it there, if we don't find
    it on the path.

--- shell-common/interactive.sh
+++ shell-common/interactive.sh
@@ -1,7 +1,14 @@
-virtualenvwrapper_path=$(which virtualenvwrapper.sh 2>/dev/null)
-if test $? -eq 0; then
-    source "${virtualenvwrapper_path}"
+virtualenvwrapper_path=$(_find_executable virtualenvwrapper.sh)
+
+if test -z "$virtualenvwrapper_path"; then
+    test -e /usr/share/virtualenvwrapper/virtualenvwrapper.sh &&
+        virtualenvwrapper_path=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
 fi
+
+if test -n "$virtualenvwrapper_path"; then
+    . "${virtualenvwrapper_path}"
+fi
+
 unset virtualenvwrapper_path
 
 . "$ETC_HOME/shell-common/aliases.sh"
