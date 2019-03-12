81d72c288af79dea7a74df191c5cd3a45df83046
John Szakmeister <john@szakmeister.net>
2019-03-12 07:39:13 -0400

    shell-common: guard run_detached with a check for nohup

--- shell-common/functions.sh
+++ shell-common/functions.sh
@@ -156,12 +156,14 @@ function find-domain-controllers()
     dig $DNS_SERVER $OPTIONS -t SRV _ldap._tcp.$1
 }
 
-# Runs a command and detaches it from the terminal.  It also silences stdout and
-# stderr, getting rid of debugging from many GTK related projects.
-function run_detached()
-{
-    nohup "$@" </dev/null >/dev/null 2>&1 &
-}
+if _has_executable nohup; then
+    # Runs a command and detaches it from the terminal.  It also silences stdout
+    # and stderr, getting rid of debugging from many GTK related projects.
+    function run_detached()
+    {
+        nohup "$@" </dev/null >/dev/null 2>&1 &
+    }
+fi
 
 # Some git-related setup for completion.
 
