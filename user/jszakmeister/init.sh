ee7b83956b42aedd712e1c3a5bfd17e4ad5d9ccc
John Szakmeister <john@szakmeister.net>
2022-06-29 19:36:39 -0400

    user/jszakmeister: add a function to dump ssh hostnames

--- user/jszakmeister/init.sh
+++ user/jszakmeister/init.sh
@@ -374,6 +374,12 @@ td()
     touch "$1"
 }
 
+function ssh-hosts()
+{
+    local _ssh_hosts=$(sed -ne 's/^[\t]*[Hh][Oo][Ss][Tt]\([Nn][Aa][Mm][Ee]\)\{0,1\}['"$'\t '"']\{1,\}\([^#*?]*\)\(#.*\)\{0,1\}$/\2/p' ~/.ssh/config | xargs -n 1 echo)
+    echo "$_ssh_hosts" | xargs -n 1 echo
+}
+
 if _has_executable ag; then
     ag()
     {
