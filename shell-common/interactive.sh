c934bb808b6549a70d79403050e40e56d6c91114
John Szakmeister <john@szakmeister.net>
2022-06-30 12:38:24 -0400

    shell-common: only set VIRTUALENVWRAPPER_PYTHON if it's not set already

--- shell-common/interactive.sh
+++ shell-common/interactive.sh
@@ -1,20 +1,25 @@
 virtualenvwrapper_path=$(_find_executable virtualenvwrapper.sh)
 
-if test -z "$virtualenvwrapper_path"; then
+if test -z "$virtualenvwrapper_path"
+then
     test -e /usr/share/virtualenvwrapper/virtualenvwrapper.sh &&
         virtualenvwrapper_path=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
 fi
 
-if test -n "$virtualenvwrapper_path"; then
-    # Cope with virtualenvwrapper being installed under Python 3 on Catalina.
-    if [ -z "${virtualenvwrapper_path##*Python/3*}" ]; then
-        export VIRTUALENVWRAPPER_PYTHON="$(_find_executable python3)"
-    elif ! _has_executable python
+if test -n "$virtualenvwrapper_path"
+then
+    if [ -z "$VIRTUALENVWRAPPER_PYTHON" ]
     then
-        if _has_executable python3
-        then
-            # Can't find python (undecorated) so, let's shoot for python3
+        # Cope with virtualenvwrapper being installed under Python 3 on Catalina.
+        if [ -z "${virtualenvwrapper_path##*Python/3*}" ]; then
             export VIRTUALENVWRAPPER_PYTHON="$(_find_executable python3)"
+        elif ! _has_executable python
+        then
+            if _has_executable python3
+            then
+                # Can't find python (undecorated) so, let's shoot for python3
+                export VIRTUALENVWRAPPER_PYTHON="$(_find_executable python3)"
+            fi
         fi
     fi
 
