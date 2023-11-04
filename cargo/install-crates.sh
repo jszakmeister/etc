be4f970d4e3dc73eaf138264147ded5a271f7c57
John Szakmeister <john@szakmeister.net>
2023-11-04 06:32:05 -0400

    cargo: make a script to help install useful crates

---
+++ cargo/install-crates.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+_has_executable()
+{
+    type -P "$@" > /dev/null 2>&1
+}
+
+# Maybe filter some of these off if Brew is available, such as bat?
+CRATES=(
+    cargo-update
+    dtool
+    du-dust
+    fd-find
+    git-graph
+    git-igitt
+    git-stack
+    names
+    recursum
+    skim
+    xcp
+)
+
+# If we have brew, use the brew versions instead.
+if ! _has_executable brew
+then
+    CRATES+=(
+        bat
+        bottom
+        broot
+        eza
+        fcp
+        git-delta
+        just
+        ripgrep
+        rust-parallel
+        sd
+        topgrade
+    )
+fi
+
+cargo install "${CRATES[@]}"
