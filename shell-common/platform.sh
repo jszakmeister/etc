552b5a879149fb8bf5a95e04dbb6b4c3c443901c
John Szakmeister <john@szakmeister.net>
2024-01-02 18:09:16 -0500

    shell-common: a little more cleanup on the platform bits

--- shell-common/platform.sh
+++ shell-common/platform.sh
@@ -1,17 +1,21 @@
-# A handy variable to detect which _etc_platform we're running on ('linux', 'darwin',
+# A handy variable to detect which platform we're running on ('linux', 'darwin',
 # 'mingw', etc.).
 _etc_machine="$(uname -m | tr '[:upper:]' '[:lower:]')"
-_etc_platform=$(uname -s | tr '[:upper:]' '[:lower:]')
-if [[ $_etc_platform == mingw* ]]; then
+_etc_platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
+if [[ $_etc_platform == mingw* ]]
+then
     _etc_platform=mingw
 fi
 
-if [ "$_etc_platform" == 'freebsd' -o "$_etc_platform" == 'darwin' ]; then
+if [ "$_etc_platform" = "freebsd" -o "$_etc_platform" = "darwin" ]
+then
     function _num_cpus()
     {
         sysctl -n hw.ncpu
     }
-elif [ "$_etc_platform" == 'linux' ]; then
+elif [ "$_etc_platform" = 'linux' ]
+then
     function _num_cpus()
     {
         grep -c ^processor /proc/cpuinfo
