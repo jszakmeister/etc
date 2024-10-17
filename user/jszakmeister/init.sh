26275cff306b841d49d73075c454bd3ab2db08c5
John Szakmeister <john@szakmeister.net>
2024-10-17 11:06:38 -0400

    user/jszakmeister: don't colorize `-h`
    Too many utilities assign other meanings to `-h`.

--- user/jszakmeister/init.sh
+++ user/jszakmeister/init.sh
@@ -582,7 +582,7 @@ then
 
     if [ -n "$ZSH_VERSION" ]
     then
-        alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+        # alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
         alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
     fi
 else
