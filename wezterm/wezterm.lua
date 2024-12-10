dbbefbd43c780bbff968965de2ff10ae39b00008
John Szakmeister <john@szakmeister.net>
2024-12-10 17:02:54 -0500

    wezterm: allow for a local_config module to apply local customizations

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -20,5 +20,19 @@ config.enable_scroll_bar = true
 
 config.color_scheme = 'iTerm2 Tango Dark'
 
+-- If you have local configuration changes, create
+-- ~/.config/wezterm/local_config.lua and put the following into it:
+--   local mod = {}
+--
+--   function mod.update_config(config)
+--       ### config updates go here
+--   end
+--
+--   return mod
+has_local, local_config = pcall(require, "local_config")
+if has_local then
+    local_config.update_config(config)
+end
+
 -- and finally, return the configuration to wezterm
 return config
