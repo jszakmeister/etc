408b2d945428ed5d966043ac31134844a17b694a
John Szakmeister <john@szakmeister.net>
2024-12-24 12:46:00 -0500

    cargo: include dysk on Linux

--- cargo/install-crates.sh
+++ cargo/install-crates.sh
@@ -5,6 +5,8 @@ has_executable()
     type -P "$@" > /dev/null 2>&1
 }
 
+platform="$(uname -s | tr '[:upper:]' '[:lower:]')"
+
 # Maybe filter some of these off if Brew is available, such as bat?
 CRATES=(
     cargo-update
@@ -14,11 +16,22 @@ CRATES=(
     git-graph
     git-igitt
     git-stack
+    hwatch
+    lychee
+    mcfly
     names
     recursum
+    riffdiff
     skim
     xcp
 )
+
+if [ "$platform" = "linux" ]
+then
+    CRATES+=(
+        dysk
+    )
+fi
 
 # If we have brew, use the brew versions instead.
 if ! has_executable brew
